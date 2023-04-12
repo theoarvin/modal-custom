4980958e9355aa9e90f0ba2baec934365f609b0b
theoarvin <arvintheo69@gmail.com>
2023-04-12 14:18:40 +0200

    push npm package

---
+++ src/Modal.js
@@ -0,0 +1,71 @@
+import React, { useRef } from "react";
+
+function Modal({  openModal,setOpenModal }) {
+  const modalRef = useRef(null);
+
+  const closeModal = () => {
+    setOpenModal(false)
+  };
+  if(!openModal){
+    return null
+  }
+  
+  return (
+    <div
+      ref={modalRef}
+      style={{
+        position: "absolute",
+        display: "flex",
+        top: 0,
+        bottom: 0,
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "RGBA(0,0,0,0.71)",
+        zIndex: 1,
+      }}
+      className="modal"
+    >
+      <div
+        className="modal-text"
+        style={{
+          position: "absolute",
+          display: "flex",
+          zIndex: 1000,
+          width: "40%",
+          height: "45px",
+          alignItems: "center",
+          paddingLeft: "35px",
+          borderRadius: "8px",
+          backgroundColor: "#fff",
+          bottom: "400px",
+          left: "50%",
+          transform: "translate(-50%)",
+        }}
+      >
+        <p>Employee Created!</p>
+        <span
+          className="modal-btn-close"
+          style={{
+            width: "30px",
+            height: "30px",
+            backgroundColor: "#000",
+            position: "absolute",
+            right: "-10px",
+            top: "-10px",
+            borderRadius: "50%",
+            color: "#fff",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "20px",
+            cursor: "pointer",
+          }}
+        >
+          <strong onClick={() => closeModal()}>X</strong>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { Counter } from "./counter";
+export { default as Modal } from "./Modal";
 
